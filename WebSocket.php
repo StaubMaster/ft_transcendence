d0e6896d868ad6d3329340610e8aa187622b9ac8
StaubMaster <dgerold@student.42heilbronn.de>
2025-02-18 15:13:27 +0100

    added Frame class

---
+++ WebSocket.php
@@ -0,0 +1,204 @@
+
+<?php
+
+class Frame
+{
+	const OpenCode_ContinuationFrame  = 0x0;
+	const OpenCode_TextFrame          = 0x1;
+	const OpenCode_BinaryFrame        = 0x2;
+	#ReservedNonControl = 0x3
+	#ReservedNonControl = 0x4
+	#ReservedNonControl = 0x5
+	#ReservedNonControl = 0x6
+	#ReservedNonControl = 0x7
+	const OpenCode_ConnectionClose    = 0x8;
+	const OpenCode_Ping               = 0x9;
+	const OpenCode_Pong               = 0xA;
+	#ReservedControl    = 0xB
+	#ReservedControl    = 0xC
+	#ReservedControl    = 0xD
+	#ReservedControl    = 0xE
+	#ReservedControl    = 0xF
+
+	private $fin = 1;
+	private $rsv = 0;
+	private $code;
+
+	private $mask_bit = 0;
+	private $mask_arr = null;
+
+	private $payload_len = 0;
+	private $payload_str = null;
+
+	public function setOpenCode($code)
+	{
+		$this->code = $code;
+	}
+	public function getOpenCode()
+	{
+		return $this->code;
+	}
+
+	public function setPayload($payload)
+	{
+		$this->payload_str = $payload;
+	}
+	public function getPayload()
+	{
+		return $this->payload_str;
+	}
+
+	public function recv($socket)
+	{
+		if (($buf = socket_read($socket, 2, PHP_BINARY_READ)) === false)
+		{
+			echo "socket_read(): " . socket_strerror(socket_last_error($socket)) . "\n";
+		}
+	
+		$byte = ord($buf[0]);
+		$this->fin  = $byte & 0b10000000;
+		$this->rsv  = $byte & 0b01110000;
+		$this->code = $byte & 0b00001111;
+	
+		$byte = ord($buf[1]);
+		$this->mask_bit    = $byte & 0b10000000;
+		$this->payload_len = $byte & 0b01111111;
+
+		if ($this->payload_len == 126)
+		{
+			if (($buf = socket_read($socket, 2, PHP_BINARY_READ)) === false)
+			{
+				echo "Len16\n";
+				echo "socket_read(): " . socket_strerror(socket_last_error($socket)) . "\n";
+			}
+	
+			$this->payload_len = 0;
+			for ($i = 0; $i < 2; $i++)
+			{
+				$byte = ord($buf[$i]);
+				$this->payload_len = ($this->payload_len << 8) | $byte;
+			}
+		}
+		elseif ($this->payload_len == 127)
+		{
+			if (($buf = socket_read($socket, 8, PHP_BINARY_READ)) === false)
+			{
+				echo "Len64\n";
+				echo "socket_read(): " . socket_strerror(socket_last_error($socket)) . "\n";
+			}
+	
+			$this->payload_len = 0;
+			for ($i = 0; $i < 8; $i++)
+			{
+				$byte = ord($buf[$i]);
+				$this->payload_len = ($this->payload_len << 8) | $byte;
+			}
+		}
+	
+		if (($buf = socket_read($socket, 4, PHP_BINARY_READ)) === false)
+		{
+			echo "Mask\n";
+			echo "socket_read(): " . socket_strerror(socket_last_error($socket)) . "\n";
+		}
+		
+		$mask_arr = array(0, 0, 0, 0);
+		for ($i = 0; $i < 4; $i++)
+		{
+			$mask_arr[$i] = ord($buf[$i]);
+		}
+	
+		if (($buf = socket_read($socket, $this->payload_len, PHP_BINARY_READ)) === false)
+		{
+			echo "payload\n";
+			echo "socket_read(): " . socket_strerror(socket_last_error($socket)) . "\n";
+		}
+
+		for ($i = 0; $i < $this->payload_len; $i++)
+		{
+			$byte = ord($buf[$i]);
+			$byte = ($byte ^ $mask_arr[$i % 4]);
+			$buf[$i] = chr($byte);
+		}
+
+		$this->payload_str = $buf;
+	}
+	public function send($socket)
+	{
+		$this->payload_len = strlen($this->payload_str);
+
+		$buf = "";
+
+		$byte = 0;
+		$byte |= $this->fin << 7;
+		$byte |= $this->rsv << 4;
+		$byte |= $this->code;
+		$buf .= chr($byte);
+		
+		$byte = 0;
+		$byte |= $this->mask_bit << 7;
+		if ($this->payload_len > 127)
+		{
+			$byte |= 126;
+			$buf .= chr($byte);
+			
+			$len = $this->payload_len;
+			for ($i = 0; $i < 2; $i++)
+			{
+				$byte = chr($len >> 8);
+				$buf .= chr($byte);
+				$len = $len << 8;
+			}
+		}
+		else
+		{
+			$byte |= $this->payload_len;
+			$buf .= chr($byte);
+		}
+
+		$buf .= $this->payload_str;
+
+		socket_write($socket, $buf);
+	}
+}
+
+class WebSocket
+{
+	public static function HandShake($fkey)
+	{
+		$faccept = $fkey . "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+		$faccept = sha1($faccept, true);
+		$faccept = base64_encode($faccept);
+		return $faccept;
+	}
+
+	private $socket;
+	private $active;
+
+	function __construct($sock)
+	{
+		$this->socket = $sock;
+		$this->active = true;
+	}
+
+	function isActive()
+	{
+		return $active;
+	}
+	
+
+	public function recv()
+	{
+		$frame = new Frame();
+		$frame->recv($this->socket);
+		return $frame->getPayload();
+	}
+	public function send($payload)
+	{
+		$frame = new Frame();
+		$frame->setOpenCode(Frame::OpenCode_TextFrame);
+		$frame->setPayload($payload);
+		$frame->send($this->socket);
+	}
+}
+
+?>
--- server.php
+++ server.php
@@ -6,20 +6,119 @@ error_reporting(E_ALL);
 ob_implicit_flush();
 
 include 'socket_help.php';
+include 'WebSocket.php';
 
 
-function WebSocket_HandShake($fkey)
+
+/*function WebSocket_recv($fsocket)
 {
-	$faccept = $fkey . "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-	$faccept = sha1($faccept, true);
-	$faccept = base64_encode($faccept);
-	return $faccept;
-}
+	if (($buf = socket_read($fsocket, 2, PHP_BINARY_READ)) === false)
+	{
+		echo "socket_read(): " . socket_strerror(socket_last_error($fsocket)) . "\n";
+	}
 
+	$byte = ord($buf[0]);
+	$fin  = $byte & 0b10000000;
+	$rsv  = $byte & 0b01110000;
+	$code = $byte & 0b00001111;
+	//echo "fin  '$fin'\n";
+	//echo "rsv  '$rsv'\n";
+	//echo "code '$code'\n";
 
-if (($server_socket = socket_server_create('127.0.0.1', 5000)) === false)
+	$byte = ord($buf[1]);
+	$mask_bit    = $byte & 0b10000000;
+	$payload_len = $byte & 0b01111111;
+	//echo "mask_bit    '$mask_bit'\n";
+	//echo "payload_len '$payload_len'\n";
+
+	if ($payload_len == 126)
+	{
+		if (($buf = socket_read($fsocket, 2, PHP_BINARY_READ)) === false)
+		{
+			echo "Len16\n";
+			echo "socket_read(): " . socket_strerror(socket_last_error($fsocket)) . "\n";
+		}
+
+		$payload_len = 0;
+		for ($i = 0; $i < 2; $i++)
+		{
+			$byte = ord($buf[$i]);
+			echo "[$i] '$byte'\n";
+			$payload_len = ($payload_len << 8) | $byte;
+		}
+		//echo "payload_len '$payload_len'\n";
+	}
+	elseif ($payload_len == 127)
+	{
+		if (($buf = socket_read($fsocket, 8, PHP_BINARY_READ)) === false)
+		{
+			echo "Len64\n";
+			echo "socket_read(): " . socket_strerror(socket_last_error($fsocket)) . "\n";
+		}
+
+		$payload_len = 0;
+		for ($i = 0; $i < 8; $i++)
+		{
+			$byte = ord($buf[$i]);
+			$payload_len = ($payload_len << 8) | $byte;
+		}
+		//echo "payload_len '$payload_len'\n";
+	}
+
+	if (($buf = socket_read($fsocket, 4, PHP_BINARY_READ)) === false)
+	{
+		echo "Mask\n";
+		echo "socket_read(): " . socket_strerror(socket_last_error($fsocket)) . "\n";
+	}
+	
+	$mask_arr = array(0, 0, 0, 0);
+	for ($i = 0; $i < 4; $i++)
+	{
+		$mask_arr[$i] = ord($buf[$i]);
+	}
+
+	if (($buf = socket_read($fsocket, $payload_len, PHP_BINARY_READ)) === false)
+	{
+		echo "payload\n";
+		echo "socket_read(): " . socket_strerror(socket_last_error($fsocket)) . "\n";
+	}
+
+	for ($i = 0; $i < $payload_len; $i++)
+	{
+		$byte = ord($buf[$i]);
+		$byte = ($byte ^ $mask_arr[$i % 4]);
+		$buf[$i] = chr($byte);
+	}
+
+	return $buf;
+}*/
+/*function WebSocket_send($foscket, $payload)
 {
-	while (true) { }
+	$payload_len = strlen($payload);
+	$str = "";
+
+	$byte = 0b10000000 | 0x1;
+	$str .= chr($byte);
+
+	$byte = $payload_len;
+	$str .= chr($byte);
+
+	$str .= $payload;
+
+	socket_write($foscket, $str);
+}*/
+
+
+while (true)
+{
+	if (($server_socket = socket_server_create('127.0.0.1', 5000)) === false)
+	{
+		sleep(1);
+	}
+	else
+	{
+		break;
+	}
 }
 
 echo "loop\n";
@@ -88,8 +187,7 @@ do
 			else
 			{
 				echo ".... WebSocket\n";
-				$websocket_accept = WebSocket_HandShake($websocket_key);
-				echo "accelt '$websocket_accept'\n";
+				$websocket_accept = WebSocket::HandShake($websocket_key);
 				Respond101($client_socket, $websocket_accept);
 
 				if (socket_set_block($client_socket) === false)
@@ -97,100 +195,14 @@ do
 					echo "socket_set_block(): " . socket_strerror(socket_last_error($client_socket)) . "\n";
 				}
 
-				if (($str = socket_read($client_socket, 2, PHP_BINARY_READ)) === false)
-				{
-					echo "socket_read(): " . socket_strerror(socket_last_error($client_socket)) . "\n";
-				}
+				$ws = new WebSocket($client_socket);
 
-				$byte = ord($str[0]);
-				$fin  = $byte & 0b10000000;
-				$rsv  = $byte & 0b01110000;
-				$code = $byte & 0b00001111;
-				echo "fin  '$fin'\n";
-				echo "rsv  '$rsv'\n";
-				echo "code '$code'\n";
+				$message = $ws->recv();
+				echo "---> '$message'\n";
 
-				$byte = ord($str[1]);
-				$mask_bit    = $byte & 0b10000000;
-				$payload_len = $byte & 0b01111111;
-				echo "mask_bit    '$mask_bit'\n";
-				echo "payload_len '$payload_len'\n";
-
-				if ($payload_len == 126)
-				{
-					if (($str = socket_read($client_socket, 2, PHP_BINARY_READ)) === false)
-					{
-						echo "Len16\n";
-						echo "socket_read(): " . socket_strerror(socket_last_error($client_socket)) . "\n";
-					}
-
-					$payload_len = 0;
-					for ($i = 0; $i < 2; $i++)
-					{
-						$byte = ord($str[$i]);
-						echo "[$i] '$byte'\n";
-						$payload_len = ($payload_len << 8) | $byte;
-					}
-					echo "payload_len '$payload_len'\n";
-				}
-				elseif ($payload_len == 127)
-				{
-					if (($str = socket_read($client_socket, 8, PHP_BINARY_READ)) === false)
-					{
-						echo "Len64\n";
-						echo "socket_read(): " . socket_strerror(socket_last_error($client_socket)) . "\n";
-					}
-
-					$payload_len = 0;
-					for ($i = 0; $i < 8; $i++)
-					{
-						$byte = ord($str[$i]);
-						$payload_len = ($payload_len << 8) | $byte;
-					}
-					echo "payload_len '$payload_len'\n";
-				}
-
-				if (($str = socket_read($client_socket, 4, PHP_BINARY_READ)) === false)
-				{
-					echo "Mask\n";
-					echo "socket_read(): " . socket_strerror(socket_last_error($client_socket)) . "\n";
-				}
-				
-				$mask_arr = array(0, 0, 0, 0);
-				for ($i = 0; $i < 4; $i++)
-				{
-					$mask_arr[$i] = ord($str[$i]);
-				}
-
-				if (($str = socket_read($client_socket, $payload_len, PHP_BINARY_READ)) === false)
-				{
-					echo "payload\n";
-					echo "socket_read(): " . socket_strerror(socket_last_error($client_socket)) . "\n";
-				}
-
-				for ($i = 0; $i < $payload_len; $i++)
-				{
-					$byte = ord($str[$i]);
-					$byte = ($byte ^ $mask_arr[$i % 4]);
-					$str[$i] = chr($byte);
-				}
-				echo "'$str'\n";
-
-
-
-				$payload = "hiiiiiiiiii :]";
-				$payload_len = strlen($payload);
-				$str = "";
-
-				$byte = 0b10000000 | 0x1;
-				$str .= chr($byte);
-
-				$byte = $payload_len;
-				$str .= chr($byte);
-
-				$str .= $payload;
-
-				socket_write($client_socket, $str);
+				$message = "hiiiiiiiiii :]";
+				echo "<--- '$message'\n";
+				$ws->send($message);
 			}
 		}
 		else
